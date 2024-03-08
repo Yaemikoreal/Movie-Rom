c3546eac1b1eecf489f8e8575aa931a0bcd25a6a
Yaemikoreal <1036927641@qq.com>
2024-03-08 16:58:12 +0800

    【任务】更新了movie_msg和user_msg表的更新逻辑，现在，它能够正常的更新已有电影的评分内容

--- data_entry/calculate_user_msg.py
+++ data_entry/calculate_user_msg.py
@@ -17,6 +17,8 @@ class CalculateUserMsg:
         # 记录新的用户列表
         self.movie_msg_list = []
         self.user_msg_list = []
+        # 该列表用于更新表中已经存在的电影的评分
+        self.user_msg_list_old = []
         # 公共函数
         self.pf = PublicFunctions()
         # 数据库路径
@@ -71,21 +73,40 @@ class CalculateUserMsg:
         # 写入
         self.pf.write_sqlite_db(user_msg_df, 'user_msg')
 
-    def processing_movie_information(self, start_id, movie_name_list):
+    def update_movie_msg(self):
+        movie_msg_list = self.user_msg_list_old.copy()
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+        for it in movie_msg_list:
+            average_score = it.get('average_score')
+            movie_name = it.get('movie_name')
+            # 使用占位符 ? 来防止 SQL 注入，并指定要更新的列和设置的值
+            sql = "UPDATE movie_msg SET average_score = ? WHERE movie_name = ?"
+            # 执行 SQL 更新语句
+            cursor.execute(sql, (average_score, movie_name))
+        # 提交事务
+        conn.commit()
+        cursor.close()
+        conn.close()
+        print('Movie_msg表已有电影评分已更新！')
+
+    def processing_movie_information(self, start_id, movie_name_list, movie_msg_df):
         for movie in movie_name_list:
             movie_status = self.inspect_movie_name(movie)
+            # 计算出该电影的平均评分
+            filtered_df = self.movie_comments_df[self.movie_comments_df['movie_name'] == movie].copy()
+            average_score = filtered_df['movie_rating'].mean()
+            average_score = round(average_score, 1)
+            start_id += 1
+            movie_dt = {
+                "movie_id": start_id,
+                "movie_name": movie,
+                "average_score": average_score
+            }
             if movie_status:
-                # 计算出该电影的平均评分
-                filtered_df = self.movie_comments_df[self.movie_comments_df['movie_name'] == movie].copy()
-                average_score = filtered_df['movie_rating'].mean()
-                average_score = round(average_score)
-                start_id += 1
-                movie_dt = {
-                    "movie_id": start_id,
-                    "movie_name": movie,
-                    "average_score": average_score
-                }
                 self.movie_msg_list.append(movie_dt)
+            else:
+                self.user_msg_list_old.append(movie_dt)
 
     def calculate_movie_msg(self):
         # 读取出数据内容
@@ -95,11 +116,12 @@ class CalculateUserMsg:
         # 获取不重复的 movie_name 列并转换为列表
         movie_name_list = self.movie_comments_df['movie_name'].unique().tolist()
         # 对电影数据进行处理
-        self.processing_movie_information(start_id, movie_name_list)
+        self.processing_movie_information(start_id, movie_name_list, movie_msg_df)
         user_msg_df = pd.DataFrame(self.movie_msg_list)
-        # # 写入
+        #  写入
         self.pf.write_sqlite_db(user_msg_df, 'movie_msg')
-
+        # 更新已有电影评分数据
+        self.update_movie_msg()
     @timer
     def calculate(self):
         # 对user_msg表根据movie_data_comment表数据进行同步更新
